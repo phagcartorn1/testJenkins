4d326600e38102e017cd581de85db472dbed68c0
Pai <phagcartorn@gmail.com>
2018-09-29 13:48:09 +0700

    init project

---
+++ Test/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Pai on 29/9/2561 BE.
+//  Copyright © 2561 Pai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func calculate(a:Int,b:Int)->Int{
+        return a + b
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TestTests/TestTests.swift
@@ -0,0 +1,39 @@
+//
+//  TestTests.swift
+//  TestTests
+//
+//  Created by Pai on 29/9/2561 BE.
+//  Copyright © 2561 Pai. All rights reserved.
+//
+
+import XCTest
+@testable import Test
+
+
+class TestTests: XCTestCase {
+    
+    var testView : ViewController!
+    
+    override func setUp() {
+        super.setUp()
+        testView  = ViewController()
+    }
+    
+    override func tearDown() {
+        testView = nil
+        super.tearDown()
+    }
+    
+    func testCalculate() {
+        
+        XCTAssertEqual(testView.calculate(a: 1, b: 2), 3, "calculate wrong")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
